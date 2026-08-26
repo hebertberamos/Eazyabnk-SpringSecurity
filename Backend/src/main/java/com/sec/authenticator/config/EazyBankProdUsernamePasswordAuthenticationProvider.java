d9ba96e132013c27abe3947fb810797650520a72
hebertberamos <hebertramos09@gmail.com>
2026-08-26 09:58:41 -0300

    adding profiles to differentiate prod config and dev config

--- Backend/src/main/java/com/sec/authenticator/config/EazyBankProdUsernamePasswordAuthenticationProvider.java
+++ Backend/src/main/java/com/sec/authenticator/config/EazyBankProdUsernamePasswordAuthenticationProvider.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -12,9 +13,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
-@Profile("!prod")
+@Profile("prod")
 @RequiredArgsConstructor
-public class EazyBankUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+public class EazyBankProdUsernamePasswordAuthenticationProvider implements AuthenticationProvider {
 
     private final EazyBankUserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
@@ -24,7 +25,13 @@ public class EazyBankUsernamePasswordAuthenticationProvider implements Authentic
         String username = authentication.getName();
         String rawPwd = authentication.getCredentials().toString();
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-        return new UsernamePasswordAuthenticationToken(username, rawPwd, userDetails.getAuthorities());
+
+        if(passwordEncoder.matches(rawPwd, userDetails.getPassword())) {
+            // here is possible to add othe type of validations like the age and whatever.
+            return new UsernamePasswordAuthenticationToken(username, rawPwd, userDetails.getAuthorities());
+        } else {
+            throw new BadCredentialsException("Invalid credentials...");
+        }
     }
 
     @Override
